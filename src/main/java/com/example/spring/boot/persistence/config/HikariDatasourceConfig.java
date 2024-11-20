5c504752411245e2a2b0f752be14ab6d96013ba1
jeon-jihyeon <jihyeon001@naver.com>
2024-11-20 22:01:50 +0900

    feat: HikariDatasourceConfig

---
+++ src/main/java/com/example/spring/boot/persistence/config/HikariDatasourceConfig.java
@@ -0,0 +1,22 @@
+package com.example.spring.boot.persistence.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class HikariDatasourceConfig {
+    @Bean
+    @ConfigurationProperties(prefix = "datasource.hikari")
+    public HikariConfig config() {
+        return new HikariConfig();
+    }
+
+    @Bean
+    public HikariDataSource dataSource(@Qualifier("config") HikariConfig config) {
+        return new HikariDataSource(config);
+    }
+}
